0eb755d3fd59ec917810cf964670020c2608b26e
Gasanova Tamara <solas93@mail.ru>
2024-11-07 01:40:03 +0300

    feat: add interfaces(res), start implementing user controller and user service (get and post methods)

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,18 +1,40 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Header, Param, Post } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { User } from 'src/utils/interfaces';
+import { CreateUserDto } from 'src/utils/interfaces.dto';
+
+// const a = new DataBaseStore();
 
 @Controller('user')
 export class UsersController {
     // GET 
-    // /user
-    // /user/:id (id: uuid)
+    // /user +
+    // /user/:id (id: uuid) +
     // POST
     // /user
     //  PUT
     // /user/:id (id: uuid)
     // DELETE
     // /user/:id
+
+    constructor(private readonly usersService: UsersService) {
+
+    }
+
     @Get()
+    @Header("Content-Type", "application/json")
     findAll() {
-        return ["db(create db)"]
+        return this.usersService.findAll();
+    }
+
+    @Get(':id')
+    @Header("Content-Type", "application/json")
+    findById(@Param('id') id: string) {
+     return this.usersService.findUserById(id);
+    }
+
+    @Post()
+    addUser(@Body() user: CreateUserDto): User{
+        return this.usersService.addUser(user);
     }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,43 @@
-import { Injectable } from '@nestjs/common';
+import { Body, Get, HttpException, HttpStatus, Injectable, Post } from '@nestjs/common';
+import { randomUUID } from 'crypto';
+import { User } from 'src/utils/interfaces';
+import { CreateUserDto } from 'src/utils/interfaces.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    users: User[] = [];
+
+    @Get()
+    findAll() {
+        return this.users;
+    }
+
+    @Get()
+    findUserById(id: string) {
+        const UUIDRegEx = new RegExp(/^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/);
+        const user = this.users.find((user) => user.id === id);
+        if (user && UUIDRegEx.test(id)) {
+            return user;
+        } else if (!user && UUIDRegEx.test(id)) {
+            throw new HttpException("Not found", HttpStatus.NOT_FOUND)
+        } else {
+            throw new HttpException("Bad request", HttpStatus.BAD_REQUEST)
+        }
+    }
+
+    @Post()
+    addUser(@Body() user: CreateUserDto): User {
+        const id = randomUUID();
+        const version: number = 1;
+        const createdAt: number = new Date().getMilliseconds();
+        let updatedAt: number = new Date().getMilliseconds();
+        const newUser = { id, ...user, version, createdAt, updatedAt };
+        if (!user.login || !user.password) {
+            throw new HttpException("Bad request", HttpStatus.BAD_REQUEST);
+        } else {
+            this.users.push(newUser);
+        }
+
+        return newUser;
+    }
+}
